47e6fcbf68ff239c56d15eda9253253bc87a3efd
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-11 09:47:57 +0530

    dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_a/dashboard_page.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,64 +33,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  TextEditingController userTxtCtr = TextEditingController();
-
-  TextEditingController pwdTxtCtr = TextEditingController();
-
-  String msg = "Message here";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 16, right: 16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Login',
-                style: TextStyle(fontSize: 50, color: Colors.deepPurple),
-              ),
-              Text('Username'),
-              TextField(controller: userTxtCtr),
-              Text('Password'),
-              TextField(controller: pwdTxtCtr, obscureText: true),
-              ElevatedButton(
-                onPressed: () {
-                  print(
-                    "Button clicked " + userTxtCtr.text + " " + pwdTxtCtr.text,
-                  );
-                  if (userTxtCtr.text == pwdTxtCtr.text) {
-                    msg = "Welcome " + userTxtCtr.text;
-                  } else {
-                    msg = "Invalid credentials";
-                  }
-                  print(msg);
-                  setState(() {
-                    
-                  });
-                },
-                child: Text('Login'),
-              ),
-              Text(msg),
-            ],
-          ),
-        ),
-      ),
+      home: DashboardPage(),
     );
   }
 }
